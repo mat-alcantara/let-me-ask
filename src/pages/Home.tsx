0f56bd11c7403eb2cbad512d7128f2a2894dc34f
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-06-25 06:47:45 -0300

    chore: NLW aulas

---
+++ src/pages/Home.tsx
@@ -0,0 +1,76 @@
+import { useHistory } from "react-router-dom";
+
+import illustrationImg from "../assets/illustration.svg";
+import logoImg from "../assets/logo.svg";
+import googleIconImg from "../assets/google-icon.svg";
+
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+
+import "../styles/auth.scss";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
+
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  const [roomCode, setRoomCode] = useState("");
+
+  const handleCreateRoom = async () => {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push("/rooms/new");
+  };
+
+  const handleJoinRoom = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  };
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(e) => setRoomCode(e.target.value)}
+              value={roomCode}
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,68 @@
+import { FormEvent, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import { database } from "../services/firebase";
+
+import illustrationImg from "../assets/illustration.svg";
+import logoImg from "../assets/logo.svg";
+
+import { Button } from "../components/Button";
+
+import { useAuth } from "../hooks/useAuth";
+
+import "../styles/auth.scss";
+
+export function NewRoom() {
+  const { user } = useAuth();
+  const history = useHistory();
+
+  const [newRoom, setNewRoom] = useState("");
+
+  const handleCreateRoom = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`rooms/${firebaseRoom.key}`);
+  };
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={(e) => handleCreateRoom(e)}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(e) => setNewRoom(e.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
