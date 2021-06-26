ae66f6b2f1f4f99418f0c00a7edacd808e5e721e
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-06-25 21:13:11 -0300

    chore: Checkpoint

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'light',
+  colors: {
+    background: '#29292e',
+    text: '#f8f8f8',
+    input: '#232129',
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'light',
+  colors: {
+    background: '#f8f8f8',
+    text: '#29292e',
+    input: '#fff',
+  },
+};
